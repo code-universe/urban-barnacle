0af9e3ba0a8bdfb8dcde826e43a603a48114684d
Kelvin N. Le <nguyen007@e.ntu.edu.sg>
2021-05-01 07:15:29 +0000

    Format with black

--- app/views.py
+++ app/views.py
@@ -5,12 +5,12 @@ from django.http import HttpResponse
 # View to return the static front-end code
 def index(request):
     try:
-        with open(os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')) as f:
+        with open(os.path.join(settings.REACT_APP_DIR, "build", "index.html")) as f:
             return HttpResponse(f.read())
     except FileNotFoundError:
         return HttpResponse(
             """
-            Please build the front-end using cd frontend && npm install && npm run build 
+            Please build the front-end using cd frontend && npm install && npm run build
             """,
             status=501,
         )
