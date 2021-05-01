54b5023b69d59f4ab565bfc27542d959bfdfcaf2
Kelvin N. Le <nguyen007@e.ntu.edu.sg>
2021-05-01 02:15:21 +0000

    ✨ Create backend index view to render React build file

---
+++ app/views.py
@@ -0,0 +1,16 @@
+import os
+from app import settings
+from django.http import HttpResponse
+
+# View to return the static front-end code
+def index(request):
+    try:
+        with open(os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')) as f:
+            return HttpResponse(f.read())
+    except FileNotFoundError:
+        return HttpResponse(
+            """
+            Please build the front-end using cd frontend && npm install && npm run build 
+            """,
+            status=501,
+        )
