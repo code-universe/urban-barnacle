26fe0c034f698907e6e3a9389fba3d0b45581989
wang0798 <83526573+wang0798@users.noreply.github.com>
2021-05-11 11:51:16 +0800

    Update App.js
    Convert function to class to utilise life cycle of class. Added start time on screen.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
 import { Button } from "@tableau/tableau-ui";
 
@@ -7,98 +7,122 @@ import { Button } from "@tableau/tableau-ui";
 
 let unregisterEventFn = undefined;
 
-function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [selectedSheet, setSelectedSheet] = useState(undefined);
-  const [sheetNames, setSheetNames] = useState([]);
-  const [rows, setRows] = useState([]);
-  const [headers, setHeaders] = useState([]);
-  const [dataKey, setDataKey] = useState(1);
-  const [filteredFields, setFilteredFields] = useState([]);
-  const [dashboardName, setDashboardName] = useState("");
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      selectedSheet: undefined,
+      sheetNames: [],
+      rows: [],
+      headers: [],
+      dataKey: [],
+      filteredFields: [],
+      dashboardName: '',
+      startTime: '',
+      endTime: ''
+    };
+  }
 
-  const getSelectedSheet = (sheet) => {
-    const sheetName = sheet || selectedSheet;
-    return tableau.extensions.dashboardContent.dashboard.worksheets.find(
-      (worksheet) => worksheet.name === sheetName
-    );
-  };
-
-  const loadSelectedMarks = (sheet) => {
-    if (unregisterEventFn) {
-      unregisterEventFn();
-    }
-
-    const worksheet = getSelectedSheet(sheet);
-    worksheet.getSelectedMarksAsync().then((marks) => {
-      // Get the first DataTable for our selected marks (usually there is just one)
-      const worksheetData = marks.data[0];
-
-      // Map our data into the format which the data table component expects it
-      const rows = worksheetData.data.map((row) =>
-        row.map((cell) => cell.formattedValue)
-      );
-      const headers = worksheetData.columns.map((column) => column.fieldName);
-
-      setRows(rows);
-      setHeaders(headers);
-      setDataKey(Date.now());
-      setIsLoading(false);
+  // when component is mounted
+  componentDidMount() {
+    this.setState({
+      startTime: new Date().toLocaleString()
     });
-
-    unregisterEventFn = worksheet.addEventListener(
-      tableau.TableauEventType.MarkSelectionChanged,
-      () => {
-        setIsLoading(true);
-        loadSelectedMarks(sheet);
+    console.log('Start Time: '+ this.state.startTime);
+    const getSelectedSheet = (sheet) => {
+      const sheetName = sheet || this.state.selectedSheet;
+      return tableau.extensions.dashboardContent.dashboard.worksheets.find(
+        (worksheet) => worksheet.name === sheetName
+      );
+    };
+  
+    const loadSelectedMarks = (sheet) => {
+      if (unregisterEventFn) {
+        unregisterEventFn();
       }
-    );
-  };
-
-  useEffect(() => {
+      const worksheet = getSelectedSheet(sheet);
+      worksheet.getSelectedMarksAsync().then((marks) => {
+        // Get the first DataTable for our selected marks (usually there is just one)
+        const worksheetData = marks.data[0];
+  
+        // Map our data into the format which the data table component expects it
+        const rows = worksheetData.data.map((row) =>
+          row.map((cell) => cell.formattedValue)
+        );
+        const headers = worksheetData.columns.map((column) => column.fieldName);
+        this.setState({
+          rows:rows,
+          headers:headers,
+          dataKey: Date.now(),
+          isLoading:false
+        })
+      });
+  
+      unregisterEventFn = worksheet.addEventListener(
+        tableau.TableauEventType.MarkSelectionChanged,
+        () => {
+          this.setState({isLoading: true});
+          loadSelectedMarks(sheet);
+        }
+      );
+    };
     tableau.extensions.initializeAsync().then(() => {
       console.log("Dashboard loaded ");
       const selectedSheet = tableau.extensions.settings.get("sheet");
       console.log(selectedSheet);
-      setSelectedSheet(selectedSheet);
+      this.setState({selectedSheet:selectedSheet});
 
       const sheetNames = tableau.extensions.dashboardContent.dashboard.worksheets.map(
         (worksheet) => worksheet.name
       );
       console.log(sheetNames);
-      setSheetNames(sheetNames);
+      this.setState({sheetNames:sheetNames});
 
-      const dashboardName = tableau.extensions.dashboardContent.dashboard.name;
-      setDashboardName(dashboardName);
-      console.log(dashboardName);
+      this.setState({dashboardName: tableau.extensions.dashboardContent.dashboard.name});
+      console.log(this.state.dashboardName);
 
       const sheetSelected = !!selectedSheet;
-      setIsLoading(sheetSelected);
+      this.setState({isLoading: sheetSelected});
 
       if (!!selectedSheet) {
         loadSelectedMarks(selectedSheet);
       }
     });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hello Tableau Extension! <br />
-        Dashboard: {dashboardName}
-        <Button
-          kind={"primary"}
-          key={"primary"}
-          style={{ marginRight: 12 }}
-          onClick={() => {
-            console.log("hello");
-          }}
-        >
-          I'm Dummy
-        </Button>
-      </header>
-    </div>
-  );
-}
+  }
 
+  // executed whenever there are updates
+  componentDidUpdate() {
+    console.log('Update time: '+new Date().toLocaleString());  
+  }
+
+  // when component is unmounted
+  componentWillUnmount() {  
+    this.setState({endTime : new Date().toLocaleString()});
+    console.log('End Time: '+this.state.endTime);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Hello Tableau Extension! <br />
+          Dashboard: {this.state.dashboardName} <br/>
+          Start Time: {this.state.startTime} <br/>
+          End Time: {this.state.endTime} <br/>
+          <Button
+            kind={"primary"}
+            key={"primary"}
+            style={{ marginRight: 12 }}
+            onClick={() => {
+              console.log("hello");
+            }}
+          >
+            I'm Dummy
+          </Button>
+        </header>
+      </div>
+    );
+  }
+};
 export default App;
